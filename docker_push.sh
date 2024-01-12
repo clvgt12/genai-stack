4e8ff3acf4d72a5a25fb63a8e84bd43b6314bc19
Chris Vitalos <cvitalos@gmail.com>
2024-01-12 00:01:52 -0500

    updated to reflect new password_store

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker login -u clvgt12 -p $(pass show docker-credential-helpers/aHR0cHM6Ly9pbmRleC5kb2NrZXIuaW8vdjEv/clvgt12)
+docker login -u clvgt12 -p $(pass show Docker/docker_hub_access_token)
 for img in api pdf_bot loader bot front-end
 do
     docker tag genai-stack-$img clvgt12/genai-stack-$img:amd64_latest
