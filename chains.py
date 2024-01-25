05ecc7a772abdedb1eaf0d2fe55f5bdf5f2ed447
Chris Vitalos <cvitalos@gmail.com>
2024-01-24 21:40:42 -0500

    added two you_got_mail related functions

--- chains.py
+++ chains.py
@@ -221,3 +221,111 @@ def generate_ticket(neo4j_graph, llm_chain, input_question):
     )
     new_title, new_question = extract_title_and_question(llm_response["answer"])
     return (new_title, new_question)
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from operator import itemgetter
+
+from langchain.schema import format_document
+from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
+from langchain_core.runnables import RunnableParallel
+from langchain.prompts.prompt import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+
+memory = ConversationBufferMemory(
+    return_messages=True, output_key="answer", input_key="question"
+)
+
+DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
+def _combine_documents(
+    docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
+):
+    doc_strings = [format_document(doc, document_prompt) for doc in docs]
+    return document_separator.join(doc_strings)
+
+def configure_email_qa_rag_chain(model, vectorstore):
+    retriever = vectorstore.as_retriever()
+
+    template = """Answer the question based only on the following context:
+    {context}
+
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | model
+        | StrOutputParser()
+    )
+
+    _template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+
+    Chat History:
+    {chat_history}
+    Follow Up Input: {question}
+    Standalone question:"""
+    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+
+    template = """Answer the question based only on the following context:
+    {context}
+
+    Question: {question}
+    """
+    ANSWER_PROMPT = ChatPromptTemplate.from_template(template)
+
+    _inputs = RunnableParallel(
+        standalone_question=RunnablePassthrough.assign(
+            chat_history=lambda x: get_buffer_string(x["chat_history"])
+        )
+        | CONDENSE_QUESTION_PROMPT
+        | model
+        | StrOutputParser(),
+    )
+    _context = {
+        "context": itemgetter("standalone_question") | retriever | _combine_documents,
+        "question": lambda x: x["standalone_question"],
+    }
+    loaded_memory = RunnablePassthrough.assign(
+        chat_history=RunnableLambda(memory.load_memory_variables) | itemgetter("history"),
+    )
+    # Now we calculate the standalone question
+    standalone_question = {
+        "standalone_question": {
+            "question": lambda x: x["question"],
+            "chat_history": lambda x: get_buffer_string(x["chat_history"]),
+        }
+        | CONDENSE_QUESTION_PROMPT
+        | model
+        | StrOutputParser(),
+    }
+    # Now we retrieve the documents
+    retrieved_documents = {
+        "docs": itemgetter("standalone_question") | retriever,
+        "question": lambda x: x["standalone_question"],
+    }
+    # Now we construct the inputs for the final prompt
+    final_inputs = {
+        "context": lambda x: _combine_documents(x["docs"]),
+        "question": itemgetter("question"),
+    }
+    # And finally, we do the part that returns the answers
+    answer = {
+        "answer": final_inputs | ANSWER_PROMPT | ChatOpenAI(),
+        "docs": itemgetter("docs"),
+    }
+# And now we put it all together!
+    final_chain = loaded_memory | standalone_question | retrieved_documents | answer
+    return(final_chain)
+
+def save_memory(question, result):
+    memory.save_context({"question": question}, {"answer": result["answer"].content})
+    memory.load_memory_variables({})
+
+def ask_question(question, final_chain):
+    inputs = {"question": question}
+    result = final_chain.invoke(inputs)
+    save_memory(question, result)
+    return(result)
